7c15a2ecb6d68b1b9fb154f2cc162782ccb6df56
Mithun Halder <mithun.tutorial.one@gmail.com>
2020-08-26 15:56:44 +0600

    Login completed

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Providers\RouteServiceProvider;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
-class LoginController extends Controller
-{
+class LoginController extends Controller {
     /*
-    |--------------------------------------------------------------------------
+    |-----------------------------------------P---------------------------------
     | Login Controller
     |--------------------------------------------------------------------------
     |
@@ -17,7 +18,7 @@ class LoginController extends Controller
     | redirecting them to your home screen. The controller uses a trait
     | to conveniently provide its functionality to your applications.
     |
-    */
+     */
 
     use AuthenticatesUsers;
 
@@ -26,15 +27,50 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    // protected $redirectTo = RouteServiceProvider::HOME;
 
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct()
-    {
-        $this->middleware('guest')->except('logout');
+
+    public function attemptLogin( Request $request ) {
+        $token = $this->guard()->attempt( $this->credentials( $request ) );
+
+        if ( !$token ) {
+            return false;
+        }
+
+        $user = $this->guard()->user();
+        if ( $user instanceof MustVerifyEmail && !$user->hasVerifiedEmail() ) {
+            return false;
+        }
+
+        $this->guard()->setToken( $token );
+
+        return true;
+    }
+
+    public function sendLoginResponse( Request $request ) {
+        $this->clearLoginAttempts( $request );
+        $token = (string) $this->guard()->getToken();
+        $expiration = $this->guard()->getPayload()->get( 'exp' );
+
+        return response()->json( [
+            'token'      => $token,
+            'token_type' => 'bearer',
+            'ecpires_in' => $expiration,
+        ] );
+    }
+
+    public function sendLoginFailedResponse( Request $request ) {
+        $user = $this->guard()->user();
+
+        if ( $user instanceof MustVerifyEmail && !$user->hasVerifiedEmail() ) {
+            return response()->json( ['errors' => ['verification', 'You must verify your email account']] );
+        }
+
+        throw ValidationException::withMessages( [$this->username() => 'Invalid Credentials'] );
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -9,11 +9,10 @@ Route::group( ['middleware' => ['auth:api']], function () {
 } );
 
 //Route Group for guest only
-Route::group( ['middleware' => ['guest:api']], function () {
-    Route::group( ['namespace' => 'Auth'], function () {
-        Route::post( 'register', 'RegisterController@register' );
-        Route::post( 'verification/verify', 'VerificationController@verify' )->name( 'verification.verify' );
-        Route::post( 'verification/resend', 'VerificationController@resend' );
-    } );
+Route::group( ['middleware' => ['guest:api'], 'namespace' => 'Auth'], function () {
+    Route::post( 'register', 'RegisterController@register' );
+    Route::post( 'verification/verify', 'VerificationController@verify' )->name( 'verification.verify' );
+    Route::post( 'verification/resend', 'VerificationController@resend' );
+    Route::post( '/login', 'LoginController@login' );
 
 } );
